172da4a7862fe5e73eefb9d4122dc1279ca98dc9
Kolya22kp <142415719+Kolya22kp@users.noreply.github.com>
2024-02-29 16:57:47 +0300

    Create setpoint.php

---
+++ setpoint.php
@@ -0,0 +1,27 @@
+<?php 
+
+header('Access-Control-Allow-Origin: *');
+
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+
+header('Access-Control-Allow-Credentials: true');
+
+header('Access-Control-Allow-Headers: Authorization, Origin, X-Requested-With, Accept, X-PINGOTHER, Content-Type');
+
+$x = $_GET["x"];
+$y = $_GET["y"];
+
+$color = ["red", "green", "blue", "black", "purple", "yellow", "aqua", "orange"];
+
+$array = [
+  "x" => $x,
+  "y" => $y,
+  "color" => $color[rand(0, 7)],
+];
+
+$array_json = file_get_contents("points.json");
+$array_decode_json = json_decode($array_json, true);
+$result = array_merge($array_decode_json, $array);
+file_put_contents(json_encode($result));
+
+?>
