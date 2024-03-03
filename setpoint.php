df5072ecfdc5a629d405bd75e38b38226be5113f
Kolya22kp <142415719+Kolya22kp@users.noreply.github.com>
2024-03-03 11:58:34 +0300

    Update setpoint.php

--- setpoint.php
+++ setpoint.php
@@ -1,4 +1,4 @@
-<?php 
+/* <?php 
 
 header('Access-Control-Allow-Origin: *');
 
@@ -24,4 +24,34 @@ $array_decode_json = json_decode($array_json, true);
 $array_decode_json[] = $array;
 file_put_contents('points.json', json_encode($array_decode_json));
 
-?>
+?> */
+
+<?php
+
+// Шапка
+
+header('Access-Control-Allow-Origin: *');
+
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+
+header('Access-Control-Allow-Credentials: true');
+
+header('Access-Control-Allow-Headers: Authorization, Origin, X-Requested-With, Accept, X-PINGOTHER, Content-Type');
+
+if (isset($_GET['x']) && isset($_GET['y'])) {
+  $x = $_GET['x'];
+  $y = $_GET['y'];
+  $jsonpoints = file_get_contents('points.json');
+  $jsonpointsarray = json_decode($jsonpoints, true);
+  $newPoint = [
+        'x' => $x,
+        'y' => $y,
+        'color' => 'black',
+   ];
+  $jsonpointsarray[] = $newPoint;
+  $pointsjson = json_encode($jsonpointsarray);
+  file_put_contents('points.json', $pointsjson);
+
+} else {
+  error_log("NO X any Y");
+}
